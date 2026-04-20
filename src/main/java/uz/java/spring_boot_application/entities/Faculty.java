e859e8c5d4b7ce47e18b0b6ab065a6e32b23c8b3
bekhruzbey <bexruzizzatullayev@gmail.com>
2026-04-20 11:59:47 +0500

    some changes

--- src/main/java/uz/java/spring_boot_application/entities/Faculty.java
+++ src/main/java/uz/java/spring_boot_application/entities/Faculty.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Table(name = "faculties")
 public class Faculty extends Auditable {
 
-    @Column(unique = true, /*nullable = false*/  length = 100, name = "nomi")
+    @Column(unique = true, /*nullable = false*/  length = 100)
     private String name;
 
     private String address;
